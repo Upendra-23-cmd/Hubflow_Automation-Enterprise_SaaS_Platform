54e8d68ef0a17d88ea7dd51300938d8497839e33
upendra-23-cmd <vrambharat05@icloud.com>
2026-09-15 17:38:50 +0530

    terrafrom file added

---
+++ Backend_Phase_2/terraform/terraform.tfvars
@@ -0,0 +1,15 @@
+namespace = "hubflow"
+
+app_image = "your-dockerhub-username/hubflow:latest"
+
+app_replicas = 2
+
+mysql_database = "hubflow_db"
+
+mysql_user = "hubflow_user"
+
+mysql_password = "hubflow_pass"
+
+mysql_root_password = "rootpassword"
+
+jwt_secret = "hubflow_prod_secret_jwt_key_2026"
---
+++ Backend_Phase_2/terraform/variables.tf
@@ -0,0 +1,38 @@
+variable "namespace" {
+  type    = string
+  default = "hubflow"
+}
+
+variable "app_image" {
+  type = string
+}
+
+variable "app_replicas" {
+  type    = number
+  default = 2
+}
+
+variable "mysql_database" {
+  type    = string
+  default = "hubflow_db"
+}
+
+variable "mysql_user" {
+  type    = string
+  default = "hubflow_user"
+}
+
+variable "mysql_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "mysql_root_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "jwt_secret" {
+  type      = string
+  sensitive = true
+}
